6100952efd3db657372568f7d0f630a5021b338d
AbMohiz <abmohiz22@gmail.com>
2023-09-27 22:21:40 +0500

    added test_BankingSystem2 to the main directory

---
+++ test_BankingSystem2.py
@@ -0,0 +1,15 @@
+# import pytest
+
+from Bankingsystem2 import Bank
+
+
+def test_deposit():
+    # Create a Bank instance
+    user = Bank("John", 10001, 25, "M")
+
+    # Deposit some amount
+    user.deposit(1000)
+
+    # Check if the balance is updated correctly
+    assert user.balance == 1000
+
