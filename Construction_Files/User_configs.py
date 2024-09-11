836b214430e957f63e7b7f0a31c8daf5136ed439
abmohiz <abmohiz22@gmail.com>
2024-09-11 22:08:46 +0500

    Refactored with making each detail object private and adding instance to specify their type.

--- Construction_Files/User_configs.py
+++ Construction_Files/User_configs.py
@@ -2,23 +2,31 @@
 Represents a customer in the banking system.
 '''
 class User:
-    def __init__(self, name, account_number, age, gender):
-        self.name = name
-        self.account_number = int(account_number)
-        self.age = age
+    def __init__(self, name: str, account_number:int , age:int , gender:str):
+        self._name = name
+        self._account_number = int(account_number)
+        self._age = age
         self.gender = gender
+
+        if not isinstance(name, str) or not name.strip():
+            raise ValueError("Name must be string")
+
+        if not isinstance(account_number, int) :
+            raise ValueError("Account number must be a number value")
+
+        if not isinstance(age, int):
+            raise ValueError("Age must be in number value")
+
+        if gender not in ["M", "F"]:
+            raise ValueError("Gender must be M for Male and F for Female")
 
     def show_details(self):
         print("-------------------------------------")
-        print("Name of the customer is:", self.name)
-        print("Account number:", self.account_number)
-        print("Age of the customer is:", self.age)
-        if self.gender == "M":
-            print("Gender: Male")
-        elif self.gender == "F":
-            print("Gender: Female")
-        else:
-            print("Invalid gender detail given")
+        print("Name of the customer is:", self._name)
+        print("Account number:", self._account_number)
+        print("Age of the customer is:", self._age)
+        gender_dict = {"M": "Male", "F": "Female"}
+        print(f"Gender :{gender_dict.get(self.gender,"Invalid gender detail given")}")
 
 mohiz = User("ab",12,23,"M")
 mohiz.show_details()
